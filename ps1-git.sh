2618a962c905e6ff32dcd94092253d7299338030
Damir Harambasic <damir.hara@gmail.com>
2016-08-04 11:07:48 -0600

    added in additional styling fixes to ps1

--- ps1-git.sh
+++ ps1-git.sh
@@ -102,8 +102,8 @@ if [ $? -eq 0 ]; then \
   else \
     # @5 - Changes to working tree
     echo "'$IRed'"$(__git_ps1 " {%s}"); \
-  fi) '$BYellow$PathShort$Color_Off$NewLine'\$ "; \
+  fi) '$BYellow'.../'$PathFull$Color_Off$NewLine'\$ "; \
 else \
   # @2 - Prompt when not in GIT repo
-  echo " '$Yellow$PathShort$Color_Off$NewLine'\$ "; \
+  echo " '$Yellow'.../'$PathFull$Color_Off$NewLine'\$ "; \
 fi)'
