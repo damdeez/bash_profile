79eb41524307a0e1ee0860987d2eb1c8d8ebf297
Damir Harambasic <damir.hara@gmail.com>
2018-11-05 14:32:46 -0700

    added gcloud commands

--- profile.sh
+++ profile.sh
@@ -26,3 +26,9 @@ function cdf () {
     echo "cd to \"$finderPath\""
     cd "$finderPath"
 }
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/dharambasic/google-cloud-sdk/path.bash.inc' ]; then . '/Users/dharambasic/google-cloud-sdk/path.bash.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/dharambasic/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/dharambasic/google-cloud-sdk/completion.bash.inc'; fi
